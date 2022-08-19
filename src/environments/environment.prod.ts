041f1ac8b94155d76f689e1bc8ef5fe2e48d0537
Eunice Igbinedion <eunice.igbinedion@yahoo.com>
2022-08-19 23:27:21 +0200

    Updating chnages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,15 @@
 
 export const environment = {
   production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyA8EAekLTbmqTbooev3aIwDwMQSuzesUwE",
+    authDomain: "gusapp-41a08.firebaseapp.com",
+    projectId: "gusapp-41a08",
+    storageBucket: "gusapp-41a08.appspot.com",
+    messagingSenderId: "213687519086",
+    appId: "1:213687519086:web:0f99d038d356314d85237f"
+  }
+
 
 };
 
